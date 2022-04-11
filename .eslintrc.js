abd7f630fdb8c422e7b94a2f12c239aa1dbd668c
saddam-satria <ardhisaddam@gmail.com>
2022-04-11 21:30:52 +0700

    update ts parses, ts lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,27 @@
 module.exports = {
-  root: true,
+  env: {
+    es6: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:node/recommended',
+  ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-missing-import': ['off'],
+    'no-console': 'off',
+    'no-duplicate-imports': 'warn',
+    'no-unused-vars': 'warn',
+    'comma-dangle': ['off', 'never'],
+    'object-curly-spacing': ['warn', 'always'],
+  },
 };
