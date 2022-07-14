b13d1b3269b86cade502a4d67c7266c03fee9de9
saddam-satria <ardhisaddam@gmail.com>
2022-07-14 12:38:42 +0700

    update remove inherit & update message

--- src/app/controllers/welcomeController.ts
+++ src/app/controllers/welcomeController.ts
@@ -1,9 +1,28 @@
-import BaseController from '../config/controller';
+import { Request, Response } from 'express';
+import {
+  BASE_URL,
+  PORT,
+  BASE_ASSET,
+  BASE_PATH,
+  ASSET_URL,
+} from '../config/utils/constant';
 
-class WelcomeController extends BaseController {
-  public get() {
-    this.response.send('welcome to server node TS');
+class WelcomeController {
+  public get(_request: Request, response: Response) {
+    response.status(200).json({
+      message: 'welcome to server node TS boilerplate',
+      status: 'success',
+      owner: 'Saddam Satria',
+      github: 'https://github.com/saddam-satria',
+      linkedIn: 'https://www.linkedin.com/in/saddam-satria-ardhi-837570170/',
+      instagram: 'https://instagram.com/saddamsatria_12',
+      BASE_URL,
+      PORT,
+      BASE_PATH,
+      ASSET_URL,
+      BASE_ASSET,
+    });
   }
 }
 
-export default WelcomeController;
+export default new WelcomeController();
