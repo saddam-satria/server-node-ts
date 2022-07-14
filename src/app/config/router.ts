2fe027adec6c89d41064c5efb5e65c4d3eb6e949
saddam-satria <ardhisaddam@gmail.com>
2022-07-14 12:37:22 +0700

    constant variabel

--- src/app/config/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import WelcomeController from '../controllers/welcomeController';
-
-const router = Router();
-
-router.get('/', (req, res) => new WelcomeController(req, res).get());
-
-export default router;
---
+++ src/app/config/utils/constant.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+
+// Configure PORT
+export const PORT: undefined | number | string = process.env.PORT || 5000;
+
+// Configure Base URL (Server serve the URL)
+export const BASE_URL = `http://localhost:${PORT}`;
+
+// Configure Asset URL (Static Files)
+export const ASSET_URL = `${BASE_URL}/assets`;
+
+// Configure BASE PATH (root directory)
+export const BASE_PATH: string = path.join(__dirname, '../../../../');
+
+// Configure Public Path (Static Files)
+export const BASE_ASSET: string = path.join(BASE_PATH, 'public');
